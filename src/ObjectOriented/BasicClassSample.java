92238c55c0c872c1de086d0703bf14d2142735d3
ssoaora <ssoaora@gmail.com>
2024-04-11 00:06:11 +0900

    :white_check_mark: Added: Constructor

--- src/ObjectOriented/BasicClassSample.java
+++ src/ObjectOriented/BasicClassSample.java
@@ -16,12 +16,28 @@ class Dog extends Animal {
 
 class HouseDog extends Dog {
 
-  @Override // 부모 클래스의 메서드를 자식 클래스가 동일한 형태로 또다시 구현하는 행위
+  // 생성자. 클래스명과 메서드명이 같고, 리턴 타입을 정의하지 않는다(void도 사용하지 않는다.)
+  HouseDog(String name) {
+    this.setName(name);
+  }
+
+  // constructor overloading
+  HouseDog(int type) {
+    if (type == 1) {
+      this.setName("yorkshire");
+    }
+    else if (type == 2) {
+      this.setName("bulldog");
+    }
+  }
+
+  // method overriding. 부모 클래스의 메서드를 자식 클래스가 동일한 형태로 또다시 구현하는 행위
+  @Override
   void sleep() {
     System.out.println(this.name + " is sleeping in her house.");
   }
 
-  // 입력 항목이 다른 경우 동일한 이름의 메서드를 생성
+  // method overloading. 입력 항목이 다른 경우 동일한 이름의 메서드를 생성
   void sleep(int hour) {
     System.out.println(this.name + " is sleeping for " + hour + " hours.");
   }
@@ -40,10 +56,11 @@ public class BasicClassSample {
     dog.sleep();
      */
 
-    HouseDog houseDog = new HouseDog();
-    houseDog.name="Tomodachi";
+    HouseDog houseDog = new HouseDog("Tomodachi");
+    HouseDog houseDog2 = new HouseDog(1);
 
     houseDog.sleep();
     houseDog.sleep(3);
+    houseDog2.sleep();
   }
 }
